db422cff5fd28a4e5d4c9bbdd0b2712dbf32e8a4
Matthijs van der Kroon <mvdkroon@gmail.com>
2015-03-15 18:13:35 +0100

    HttpODBC now automatically detects db_schema (from connection string) as well as some refactoring the csv2sql code into a separate function

--- api/HttpODBC.py
+++ api/HttpODBC.py
@@ -1,6 +1,7 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado.log import enable_pretty_logging
 
 import sys, csv, io, argparse, ConfigParser, unicodecsv, StringIO, json
 sys.path.append("../common")
@@ -65,52 +66,21 @@ def listtables():
 def tabledata(tablename):
     insp = reflection.Inspector.from_engine(engine)
 
+    db_schema = connection_string[connection_string.rfind('/')+1:]
+
     def allowed_file(filename):
         return '.' in filename and \
                filename.rsplit('.', 1)[1] in ALLOWED_UPLOAD_EXTENSIONS
 
     if request.method == 'POST':
         # fire requests like this:
-        # curl -v -i -X POST -H "Content-Type: multipart/form-data" -F "file=@/Users/matthijs/Desktop/sdata.csv" 127.0.0.1:5001/table/testdata
+        # curl -v -i -X POST -H "Content-Type: multipart/form-data" -F "file=@/Users/matthijs/Desktop/data.csv" 127.0.0.1:5001/table/testdata
 
         file = request.files['file']
         if file and allowed_file(file.filename):
-            try:
-                conn = engine.connect()
-                trans = conn.begin()
+            return csv2sql(file, db_schema, tablename)
 
-                csv_table = table.Table.from_csv(
-                    file.stream,
-                    name=tablename,
-                    snifflimit=False,
-                    blanks_as_nulls=True,
-                    infer_types=True,
-                    no_header_row=False,
-                    encoding='utf-8'
-                )
-
-                sql_table = sql.make_table(
-                    csv_table,
-                    tablename,
-                    False, #self.args.no_constraints
-                    'test_db', #self.args.db_schema
-                    metadata
-                )
-
-                sql_table.create()
-
-                insert = sql_table.insert()
-                headers = csv_table.headers()
-
-                conn.execute(insert, [dict(zip(headers, row)) for row in csv_table.to_rows()])
-
-            except Exception as e:
-                print(e)
-            finally:
-                trans.commit()
-                conn.close()
-                file.close()
-        return ''
+        return "You either didn't POST a file, or the filetype isn't allowed"
 
     elif request.method == 'GET':
         if tablename in insp.get_table_names():
@@ -125,6 +95,44 @@ def tabledata(tablename):
             return Response(sql2csv(tablename, delimiter, quotechar), mimetype='text/csv')
         else:
             return "Not a valid tablename"
+
+def csv2sql(file, db_schema, tablename):
+    try:
+        conn = engine.connect()
+        trans = conn.begin()
+
+        csv_table = table.Table.from_csv(
+            file.stream,
+            name=tablename,
+            snifflimit=False,
+            blanks_as_nulls=True,
+            infer_types=True,
+            no_header_row=False,
+            encoding='utf-8'
+        )
+
+        sql_table = sql.make_table(
+            csv_table,
+            tablename,
+            False, #self.args.no_constraints
+            db_schema, #self.args.db_schema
+            metadata
+        )
+
+        sql_table.create()
+
+        insert = sql_table.insert()
+        headers = csv_table.headers()
+
+        conn.execute(insert, [dict(zip(headers, row)) for row in csv_table.to_rows()])
+
+    except Exception as e:
+        return e
+    finally:
+        trans.commit()
+        conn.close()
+        file.close()
+        return json.dumps('.'.join([db_schema, tablename]))
 
 def sql2csv(name, delimiter, quotechar):
     insp = reflection.Inspector.from_engine(engine)
@@ -143,9 +151,13 @@ def sql2csv(name, delimiter, quotechar):
         if row is None:
             break
         writer.writerow(row)
-    connection.close()
 
-    return output.getvalue()
+    retval = output.getvalue()
+
+    connection.close()
+    output.close()
+
+    return retval
 
 ALLOWED_UPLOAD_EXTENSIONS = set(['csv'])
 
@@ -167,6 +179,7 @@ if __name__ == '__main__':
 
     engine, metadata = sql.get_connection(connection_string)
 
+    enable_pretty_logging()
     server = HTTPServer(WSGIContainer(app), max_buffer_size=4000*1024*1024)
     server.bind(args.port)
     server.start(0)  # Forks multiple sub-processes
