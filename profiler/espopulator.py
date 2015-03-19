7ead4f403688f3a444b0d6746b28ed9f67585742
Matthijs van der Kroon <mvdkroon@gmail.com>
2015-03-19 12:09:52 +0100

    changed the ESpopulator to now work with cmdline args as well as some cleanup and adjustments to work with new sqlalchemy based back-end

--- profiler/espopulator.py
+++ profiler/espopulator.py
@@ -1,94 +1,89 @@
-import sys
+import sys, argparse, json, datetime
 sys.path.append("../common")
-sys.path.append("../api")
-sys.path.append("Miners")
 
+from MetaModel import MetaModel
 from objects import ForeignKey, PrimaryKey, Table, Column, Base
-import metaclient
-from MetaMiner import MetaMiner
 
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
-import pymssql
-import configparser
 
-import json
-import datetime
-
-from optparse import OptionParser
 from multiprocessing import Pool
 
-parser = OptionParser()
-parser.add_option("-i", "--host", dest="db_host", help="", metavar="string")
-parser.add_option("-u", "--user", dest="db_user", help="", metavar="string")
-parser.add_option("-p", "--password", dest="db_password", help="", metavar="string")
-parser.add_option("-c", "--catalog", dest="db_catalog", help="", metavar="string")
-parser.add_option("-d", "--db_dialect", dest="db_dialect", help="'pymssql' or 'pymysql'", metavar="string")
-# parser.add_option("-t", "--truncate", dest="truncate", help="", metavar="boolean", default=False)
-(options, args) = parser.parse_args()
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-if len(config.sections()) == 0:
-	print('config.ini file not yet present, please copy from template (templace_config.ini) and fill in required properties')
-	quit()
-
-esIp = config['es']['ip']
-esPort = config['es']['port']
-
-db_host = options.db_host
-db_user = options.db_user
-db_password = options.db_password
-db_catalog = options.db_catalog
-db_dialect = options.db_dialect
-
-metadb_connectionstring = config['metadb']['connection_string']
+from sqlalchemy import create_engine
+from sqlalchemy.sql import select
 
 excluded = ['image']
 
-def executeOne(column=None):
+def executeOne(unit=None):
+	column = unit[0]
+	esIp = unit[1]
+	esPort = unit[2]
+	connection_string = unit[3]
+
 	try:
-		if column.datatype not in excluded:
+		if column.type not in excluded:
 			es = Elasticsearch(host=str(esIp), port=str(esPort), timeout=240)
 
 			actions = []
-			miner = MetaMiner(type=db_dialect).getInstance(db_catalog=db_catalog, db_host=db_host, db_user=db_user, db_password=db_password)
-			values = miner.getDataForColumn(column=column, distinct=True)
+
+			engine = create_engine(connection_string)
+			conn = engine.connect()
+
+			s = select([column])
+			result = conn.execute(s)
+
+			values = [d[0] for d in result.fetchall()]
+
+			conn.close()
 
 			for j,value in enumerate(values):
 				actions.append({
 					"_index": "projects",
 					"_type": "fb",
 					"value": str(value),
-					"tablename": column.tablename,
-					"columnname": column.columnname,
-					"db_catalog": column.db_catalog,
-					"db_schema": column.db_schema,
-					"datatype": column.datatype
+					"tablename": column.table.name,
+					"columnname": column.name,
+					"db_catalog": column.info['db_catalog'],
+					"db_schema": column.info['schemaname'],
+					"datatype": str(column.type)
 				})
+
 			helpers.bulk(es, actions)
 	except Exception as e:
+		print("could not index column: " + str(column.name))
 		print(e)
 
 def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-i", "--ip", help="Elasticsearch host ip, defaults to 127.0.0.1", metavar="string", default='127.0.0.1')
+	parser.add_argument("-p", "--port", help="Elasticsearch host port, defaults to 9200", metavar="string", default='9200')
+	parser.add_argument("-s", "--src", help="connection_string for the src-database", metavar="string")
+	args = parser.parse_args()
+
+	esIp = args.ip
+	esPort = args.port
+	connection_string = args.src
+
 	sts = datetime.datetime.now()
 
 	pool = Pool(processes=32)
 
 	# get columns from the meta database
-	columns = metaclient.reader(metadb_connectionstring).getColumns(filter=Column.db_catalog==options.db_catalog)
-	print('## going to populate Elastic Search with {0} columns'.format(len(columns)-1))
+	miner = MetaModel(args.src)
+	units = [ (c, esIp, esPort, connection_string) for c in miner.columns() ]
+
+	print('## going to populate Elastic Search with {0} columns'.format(len(units)-1))
 	print('\n'*1)
 
 	# execute on multiple threads
-	for i, _ in enumerate(pool.imap_unordered(executeOne, columns)):
+	for i, _ in enumerate(pool.imap_unordered(executeOne, units)):
 		sys.stdout.write("\033[1A")
 
-		totalprogress = "\r\033[K## progress {0}/{1}: {2:.2f}% \n".format(i, len(columns)-1, round(i/(len(columns)-1)*100,2))
+		totalprogress = "\r\033[K## progress {0}/{1}: {2:.2f}% \n".format(i, len(units)-1, round(i/(len(units)-1)*100,2))
 		sys.stdout.write(totalprogress)
 		sys.stdout.flush()
-	
-	print('')	
+
+	print('')
 	print('## DONE.' )
 	print('## time elapsed: {0}'.format(str(datetime.datetime.now() - sts)))
 
