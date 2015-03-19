32ead35b17f442a764a83a3eefa2293b8fb6d759
Matthijs van der Kroon <mvdkroon@gmail.com>
2015-03-19 13:38:28 +0100

    ESpopulator tested and adjusted to fix issues that surfaced

--- profiler/ESpopulator.py
+++ profiler/ESpopulator.py
@@ -14,11 +14,14 @@ from sqlalchemy.sql import select
 
 excluded = ['image']
 
-def executeOne(unit=None):
-	column = unit[0]
-	esIp = unit[1]
-	esPort = unit[2]
-	connection_string = unit[3]
+def executeOne(task=None):
+	column = task[0]
+
+	esIp = task[1]
+	esPort = task[2]
+	connection_string = task[3]
+	idx = task[4]
+	typ = task[5]
 
 	try:
 		if column.type not in excluded:
@@ -32,15 +35,15 @@ def executeOne(unit=None):
 			s = select([column])
 			result = conn.execute(s)
 
-			values = [d[0] for d in result.fetchall()]
+			while True:
+			    row = result.fetchone()
+			    if row is None:
+			        break
 
-			conn.close()
-
-			for j,value in enumerate(values):
-				actions.append({
-					"_index": "projects",
-					"_type": "fb",
-					"value": str(value),
+			    actions.append({
+					"_index": idx,
+					"_type": typ,
+					"value": str(row[0]),
 					"tablename": column.table.name,
 					"columnname": column.name,
 					"db_catalog": column.info['db_catalog'],
@@ -49,37 +52,37 @@ def executeOne(unit=None):
 				})
 
 			helpers.bulk(es, actions)
+
 	except Exception as e:
 		print("could not index column: " + str(column.name))
 		print(e)
+		print('')
+	finally:
+		conn.close()
 
-def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-i", "--ip", help="Elasticsearch host ip, defaults to 127.0.0.1", metavar="string", default='127.0.0.1')
-	parser.add_argument("-p", "--port", help="Elasticsearch host port, defaults to 9200", metavar="string", default='9200')
-	parser.add_argument("-s", "--src", help="connection_string for the src-database", metavar="string")
-	args = parser.parse_args()
-
+def main(args):
 	esIp = args.ip
 	esPort = args.port
 	connection_string = args.src
+	idx = args.idx
+	typ = args.type
 
 	sts = datetime.datetime.now()
 
-	pool = Pool(processes=32)
+	pool = Pool(processes=int(args.cpu))
 
-	# get columns from the meta database
+	# get columns from the meta database and populate the worker tasks
 	miner = MetaModel(args.src)
-	units = [ (c, esIp, esPort, connection_string) for c in miner.columns() ]
+	tasks = [ (c, esIp, esPort, connection_string, idx, typ) for c in miner.columns() ]
 
-	print('## going to populate Elastic Search with {0} columns'.format(len(units)-1))
+	print('## going to populate Elastic Search with {0} columns'.format(len(tasks)-1))
 	print('\n'*1)
 
 	# execute on multiple threads
-	for i, _ in enumerate(pool.imap_unordered(executeOne, units)):
+	for i, _ in enumerate(pool.imap_unordered(executeOne, tasks)):
 		sys.stdout.write("\033[1A")
 
-		totalprogress = "\r\033[K## progress {0}/{1}: {2:.2f}% \n".format(i, len(units)-1, round(i/(len(units)-1)*100,2))
+		totalprogress = "\r\033[K## progress {0}/{1}: {2:.2f}% \n".format(i, len(tasks)-1, round(i/(len(tasks)-1)*100,2))
 		sys.stdout.write(totalprogress)
 		sys.stdout.flush()
 
@@ -88,4 +91,15 @@ def main():
 	print('## time elapsed: {0}'.format(str(datetime.datetime.now() - sts)))
 
 if __name__ == '__main__':
-	main()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-i", "--ip", help="Elasticsearch host ip, defaults to 127.0.0.1", metavar="string", default='127.0.0.1')
+	parser.add_argument("-p", "--port", help="Elasticsearch host port, defaults to 9200", metavar="string", default='9200')
+
+	parser.add_argument("-x", "--idx", help="Elasticsearch index name, defaults to 'inbox'", metavar="string", default='inbox')
+	parser.add_argument("-t", "--type", help="Elasticsearch type name, defaults to 'new'", metavar="string", default='new')
+
+	parser.add_argument("-s", "--src", help="connection_string for the src-database", metavar="string")
+	parser.add_argument("-c", "--cpu", help="number of processes to run within the pool, defaults to 4", metavar="string", default='4')
+	args = parser.parse_args()
+
+	main(args)
