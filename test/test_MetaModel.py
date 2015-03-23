c6c39715a186f10a5207876fa1a5a9ef6c4b3a08
Matthijs van der Kroon <mvdkroon@gmail.com>
2015-03-23 17:24:57 +0100

    minor changes to tests

--- test/test_MetaModel.py
+++ test/test_MetaModel.py
@@ -2,41 +2,38 @@ import sys
 sys.path.append("../profiler")
 
 import unittest
-
 import sqlite3
+
+from MetaModel import MetaModel
+
+conn = sqlite3.connect(':memory:')
+c = conn.cursor()
 
 class TestMetaModel(unittest.TestCase):
     def setUp(self):
-        conn = sqlite3.connect(':memory:')
-        c = conn.cursor()
-
         # Create table
         c.execute('''CREATE TABLE stocks
-                     (id INTEGER PRIMARY KEY AUTOINCREMENT, date text, trans text, symbol text, qty real, price real)''')
+                     (id INTEGER PRIMARY KEY AUTOINCREMENT, date text,\
+                      trans text, symbol text, qty real, price real)''')
 
-        # Insert a row of data
-        c.execute("INSERT INTO stocks VALUES (NULL, '2006-01-05','BUY','RHAT',100,35.14)")
-        c.execute("INSERT INTO stocks VALUES (NULL, '2006-01-05','BUY','RHAT',100,35.14)")
-        c.execute("INSERT INTO stocks VALUES (NULL, '2006-01-05','BUY','RHAT',100,35.14)")
-        c.execute("INSERT INTO stocks VALUES (NULL, '2006-01-05','BUY','RHAT',100,35.14)")
-        c.execute("INSERT INTO stocks VALUES (NULL, '2006-01-05','BUY','RHAT',100,35.14)")
+        for i in range(0, 100):
+            c.execute("INSERT INTO stocks VALUES (NULL, '2006-01-05','BUY',\
+                'RHAT',100,35.14)")
 
         # Save (commit) the changes
         conn.commit()
 
-        # We can also close the connection if we are done with it.
-        # Just be sure any changes have been committed or they will be lost.
+    def cleanUp(self):
+        c.execute('''DROP TABLE stocks''')
         conn.close()
 
-    def cleanUp(self):
-        conn = sqlite3.connect(':memory:')
-        c = conn.cursor()
-        c.execute('''DROP TABLE stocks''')
-
     def test(self):
-        self.setUp()
-        self.assertEqual(4, 4)
-        self.cleanUp()
+        mm = MetaModel('sqlite://')
+        print(mm.columns())
+
+
+def main():
+    unittest.main()
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
